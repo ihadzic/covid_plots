ea1ceb950b9629f0d94762c9333f35e522f2fad5
Ilija Hadzic <ilijahadzic@gmail.com>
2020-05-09 15:17:58 -0400

    calculate the moving average
    Signed-off-by: Ilija Hadzic <ilijahadzic@gmail.com>

--- pull_data.py
+++ pull_data.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import requests
 import sys
+import numpy
+
+mva_len = 7
 
 if len(sys.argv) <= 1:
     state="nj"
@@ -14,6 +17,11 @@ if r.status_code == 200:
     positives.append(0)
     positives.reverse()
     new_positives = [ x - y for (x,y) in zip(positives[1:], positives[0:-1]) ]
-    print(new_positives)
+    # moving average: implement as convolution with filters impulse
+    # response and drop the tail of last mva_len elements
+    new_positives_mva = numpy.convolve(new_positives, [1/mva_len]*mva_len)
+    new_positives_mva = new_positives_mva[:-mva_len+1]
+    print(len(new_positives))
+    print(len(new_positives_mva))
 else:
     print("API request failed {}".format(r.status_code))
