6e6a474fe89b83e0911f0178564c6038a95ac8e4
Ilija Hadzic <ilijahadzic@gmail.com>
2020-05-09 15:02:02 -0400

    calculate daily increase of positive cases
    Signed-off-by: Ilija Hadzic <ilijahadzic@gmail.com>

--- pull_data.py
+++ pull_data.py
@@ -7,5 +7,7 @@ r=requests.get(url)
 if r.status_code == 200:
     daily=r.json()
     positives=[ p.get("positive") for p in daily ]
+positives.append(0)
 positives.reverse()
-print(positives)
+new_positives = [ x - y for (x,y) in zip(positives[1:], positives[0:-1]) ]
+print(new_positives)
