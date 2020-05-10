d4aaf64f1f514252972a0810afe1ebc6b628379d
Ilija Hadzic <ilijahadzic@gmail.com>
2020-05-09 20:17:11 -0400

    fix alignment in compare_states tool
    Not all states started reporting at the same time
    so we need to right-aligh the plots. Do this by
    padding zeroes from the left of shorter data
    sets to match lengths of the longest one.
    
    Signed-off-by: Ilija Hadzic <ilijahadzic@gmail.com>

--- compare_states.py
+++ compare_states.py
@@ -11,24 +11,36 @@ if len(sys.argv) <= 2:
     exit(1)
 else:
     states = sys.argv[1:]
+
 lstates = []
+max_len = 0
+all_positives = []
+# first pass, store data locally and determine the longest data set
 for state in states:
     url="https://covidtracking.com/api/v1/states/" + state + "/daily.json"
     r= requests.get(url)
     if r.status_code == 200:
         daily=r.json()
         positives=[ p.get("positive") for p in daily ]
-        positives.append(0)
-        positives.reverse()
-        new_positives = [ x - y for (x,y) in zip(positives[1:], positives[0:-1]) ]
-        # moving average: implement as convolution with filters impulse
-        # response and drop the tail of last mva_len elements
-        new_positives_mva = numpy.convolve(new_positives, [1/mva_len]*mva_len)
-        new_positives_mva = new_positives_mva[:-mva_len+1]
-        pyplot.plot(new_positives_mva, linewidth=2)
+        if len(positives) > max_len:
+            max_len = len(positives)
+        all_positives.append(positives)
         lstates.append(state)
     else:
         print("state {} skipped (API request failed)".format(state))
+
+# second pass, pad datasets to be aligned to the latest day
+for positives in all_positives:
+    padded_positives = positives + [ 0 ] * (1 + max_len - len(positives))
+    padded_positives.reverse()
+    new_positives = [ x - y for (x,y) in zip(padded_positives[1:],
+                                             padded_positives[0:-1]) ]
+    # moving average: implement as convolution with filters impulse
+    # response and drop the tail of last mva_len elements
+    new_positives_mva = numpy.convolve(new_positives, [1/mva_len]*mva_len)
+    new_positives_mva = new_positives_mva[:-mva_len+1]
+    pyplot.plot(new_positives_mva, linewidth=2)
+
 pyplot.legend(lstates)
 pyplot.xlabel('days')
 pyplot.ylabel('cases')
